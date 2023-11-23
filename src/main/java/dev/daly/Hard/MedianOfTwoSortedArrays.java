d1f019a657992df005286da60b0b6f5f7dd8570e
DalyChouikh <chouikhdaly215@gmail.com>
2023-11-23 19:21:35 +0100

    Added comments to MedienOfTwoSortedArrays class

--- src/main/java/dev/daly/Hard/MedianOfTwoSortedArrays.java
+++ src/main/java/dev/daly/Hard/MedianOfTwoSortedArrays.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 public class MedianOfTwoSortedArrays {
     //Brute force method, not meeting time complexity requirements which is O(log(m+n))
+    //The time complexity is O(m+n) because we are iterating through both arrays
+    //The space complexity is O(m+n) because we are creating a new array to store the values of both arrays
+
     public static void main(String[] args) {
         System.out.println(findMedianSortedArrays(new int[]{1,3}, new int[]{2}));
         System.out.println(findMedianSortedArrays(new int[]{1,2}, new int[]{3,4}));
